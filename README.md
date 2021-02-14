caefa6242dce1fe1a908e3988b1a62d46789fec2
Filip Cornell <filipcornell@Filips-MBP.lan>
2021-02-14 21:00:28 +0100

    Added the first working version of this

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # img-to-chars
-Convert an img to a character image! 
+Convert an img to a character image!
+
+An example can be found in the `imgs`, where [this image]() was converted to text.
+
+## Install
+
+Do:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run
+
+To run an image, do:
+
+```
+python3 index.py --file_path=<FILE_PATH> --output_file_path=<OUTPUT_FILE_PATH>
+```
+
+where:
+- `file_path` is the path to the image to convert to a `.txt`-file.
+- `output_file_path` is the path to the output `.txt`-file.
+
+If your image is too granular, you can reduce the granularity through the `--size_reduction` argument. If you want to reduce the number of pixels in half, set it to `2` etc.
+
+## Some additional notes
+
+This is not fully tested yet! If you try to use it and it does not work, please submit an issue so I can improve it!
---
+++ index.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.image as mpimg
+import torch.nn as nn
+import torch
+import argparse
+
+CHARS = " .,-~:;=!*#$@"[::-1]
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def convert_image_to_text(
+    file_path: str, output_file_path: str = None, size_reduction: float = 1.0
+):
+    img = mpimg.imread(file_path)
+    data = rgb2gray(img)
+    data_scaled = (data - data.min()) / (data.max() - data.min())
+    scaled = nn.AvgPool2d(
+        kernel_size=(int(size_reduction * 2), int(size_reduction)),
+        stride=(int(size_reduction * 2), int(size_reduction)),
+    )(torch.Tensor(data_scaled).reshape(1, data.shape[0], data.shape[1])).numpy()[0]
+    text_arr = ""
+
+    limits = (1 / len(CHARS)) * (np.arange(len(CHARS)) + 1)
+
+    for arr in scaled:
+        app_ = []
+        for pixel in arr:
+            app_.append(CHARS[np.where(limits == limits[pixel <= limits][0])[0][0]])
+        text_arr += "".join(app_) + "\n"
+
+    # if output_file_path is None:
+    # file_name = file_path.split("/")[-1].split(".")[0]
+
+    # output_file_path = os.path.join("output", file_name + ".txt")
+    print(text_arr)
+    if output_file_path is not None:
+        with open(output_file_path, "w") as f:
+            f.write(text_arr)
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--file_path", type=str, help="Path to the image to convert. ")
+
+parser.add_argument(
+    "--output_file_path",
+    type=str,
+    default=None,
+    help="The output path for the file. Default to the name of the file. ",
+)
+
+parser.add_argument(
+    "--size_reduction",
+    type=float,
+    default=1.0,
+    help="Retrieve a more granular representation of the data through average poolings. ",
+)
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    convert_image_to_text(
+        file_path=args.file_path,
+        output_file_path=args.output_file_path,
+        size_reduction=args.size_reduction,
+    )
