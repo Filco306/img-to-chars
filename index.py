13fbc03e6d8c3ce991cf760371d4cdba6f808e57
Filip Cornell <filipcornell@Filips-MBP.lan>
2021-02-14 21:33:09 +0100

    Added source of code snippet

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import argparse
 CHARS = " .,-~:;=!*#$@"[::-1]
 
 
+# Copied from https://stackoverflow.com/questions/12201577/how-can-i-convert-an-rgb-image-into-grayscale-in-python # noqa: E501
 def rgb2gray(rgb):
     return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
 
